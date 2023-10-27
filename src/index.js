2a7cfa662b2d16a0a49f14e6232761951ffe0ef3
s.lazarenko <s.lazarenko@ukrods.com.ua>
2023-10-27 15:43:29 +0300

    commit7

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-cms-app" > 
+    <HashRouter > 
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter >
   </React.StrictMode>
 );
 
